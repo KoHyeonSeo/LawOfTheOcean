e766f8f73fad17249d144e608df4659c0ca25754
KoHyeonSeo <rhgustj01@naver.com>
2022-06-28 22:24:23 +0900

    Update Entity.cs

--- Assets/Script/Entity.cs
+++ Assets/Script/Entity.cs
@@ -4,5 +4,51 @@ using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
 {
+    [SerializeField] private float maxHealth;
+    [SerializeField] protected float health;
 
+    public virtual float Health
+    {
+        get { return health; }
+        set
+        {
+            health = value >= 0f ? value : 0f;
+            if (health > maxHealth)
+                health = maxHealth;
+        }
+    }
+
+    public float MaxHealth
+    {
+        get { return maxHealth; }
+        set
+        {
+            maxHealth = value >= 0f ? value : 0f;
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+        }
+    }
+    public bool DeadCheck { get; protected set; }
+
+    public virtual void OnEnable()
+    {
+        DeadCheck = false;
+        Health = MaxHealth;
+    }
+
+    public virtual void Heal(float healing)
+    {
+        Health += healing;
+    }
+    public virtual void Damage(float power)
+    {
+        health -= power;
+        if(health <= 0f&&!DeadCheck)
+        {
+            DeadCheck = true;
+        }
+    }
+    
 }
