9ca2a7fdd0e4aaf1f403622ddf4672afe120bbd0
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-06-29 09:04:26 +0900

    Update PlayerInput.cs

--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -8,21 +8,22 @@ public class PlayerInput : MonoBehaviour
     public const string ZAxis = "Vertical";
     public const string ShootButton = "Fire1";
     public const string YAxis = "Jump";
-    
+
     //좌: -1,정지:0,  우: 1
     public float XAxisDir { get; private set; }
     //아래: -1,정지:0,  위: 1
     public float ZAxisDir { get; private set; }
     //마우스 왼클릭시 1(true), 0(false)
     public bool IsShootingButton { get; private set; }
+    //스페이스바 사용시 1(true), 0(false)
+    public bool YAxisDir { get; private set; }
 
-    //다른 스크립트 안
-    
 
     void Update()
     {
         XAxisDir = Input.GetAxis(XAxis);
         ZAxisDir = Input.GetAxis(ZAxis);
         IsShootingButton = Input.GetButtonDown(ShootButton);
+        YAxisDir = Input.GetButtonDown(YAxis);
     }
 }
