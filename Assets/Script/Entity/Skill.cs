d5dc585251ede979bc60d189b5de0762badf3f45
Kohyeonseo <rhgustj01@naver.com>
2022-07-02 22:07:24 +0900

    update

--- Assets/Script/Entity/Skill.cs
+++ Assets/Script/Entity/Skill.cs
@@ -7,10 +7,8 @@ public abstract class Skill : ScriptableObject
     [Header("스킬 속성")]
     [SerializeField] private float power;
     [SerializeField] private float cooltime;
-    [SerializeField] private float percentage;
     public float CoolTime { get { return cooltime; } set { cooltime = value; } }
     public float Power { get { return power; } set { power = value; } }
-    public float Percentage { get { return percentage; } set { percentage = value; } }
-    
-    public virtual void UseSkill(){ }
+
+    public abstract void UseSkill();
 }
