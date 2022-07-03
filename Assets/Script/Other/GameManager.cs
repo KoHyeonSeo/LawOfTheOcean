60b4b08319b913846776feb4e4502520ee35e650
Kohyeonseo <rhgustj01@naver.com>
2022-07-03 21:02:09 +0900

    update

--- Assets/Script/Other/GameManager.cs
+++ Assets/Script/Other/GameManager.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //적 스킬 인덱스 저장할 공간 필요할거 같다.
+    //각 적의 스킬을 copy에서 사용할 시 PlayerSkillCopy에서 어떻게 사용할 수 있을까?
+
+    private int enemySkillNum;
     public static GameManager instance = new GameManager();
     private GameManager(){ }
     public static GameManager getGameManager()
     {
         return instance;
     }
-
-    [SerializeField] private int itemNum;
-
-    public int GetAllItemNum
+    //적 스킬 갯수
+    public int EnemySkillNum
     {
         get
         {
-            return itemNum;
+            return enemySkillNum;
         }
         set
         {
-            itemNum = value;
+            enemySkillNum = value < 0 ? 0 : value;
         }
     }
-
 }
