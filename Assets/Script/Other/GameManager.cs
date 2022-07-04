5974d4a5862a079cf512c2cbd96366727106912f
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-04 17:10:05 +0900

    update

--- Assets/Script/Other/GameManager.cs
+++ Assets/Script/Other/GameManager.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    public static GameManager instance = new GameManager();
-    private GameManager() { }
+    public static GameManager instance;
+    private GameManager() {
+
+        instance = this;
+    }
     public static GameManager getGameManager()
     {
         return instance;
     }
     
-    
     //copy할 수 있는 스킬의 Max 값
     [SerializeField] private int skillMaxCnt = 10;
+    //현재 스킬 갯수
+    [SerializeField] private int currentSkillCnt;
 
     [System.Serializable]
     public struct SkillInfo
@@ -25,7 +29,7 @@ public class GameManager : MonoBehaviour
     //적 스킬 인덱스(갯수) 저장할 공간 필요할거 같다.
     //각 적의 스킬을 copy에서 사용할 시 PlayerSkillCopy에서 어떻게 사용할 수 있을까?
     [Header("Player가 copy한 Enemy Skill 갯수")]
-    [SerializeField] private List<SkillInfo> skills = new List<SkillInfo>();
+    [SerializeField] private List<SkillInfo> skills;
 
     /// <summary>
     /// skill과 카피가능 갯수를 구조체에 담아 리스트 형태로 반환하는 읽기 전용 프로퍼티
@@ -33,9 +37,29 @@ public class GameManager : MonoBehaviour
     /// 0. Test1
     /// 1. Test2
     /// </summary>
+    private void Start()
+    {
+        int cnt = 0;
+        foreach(var skill in skills)
+        {
+            cnt += skill.skillCnt;
+        }
+        if(cnt > skillMaxCnt)
+        {
+            Debug.LogError("스킬 수가 Max보다 많습니다!!");
+        }
+        else
+        {
+            currentSkillCnt = cnt;
+        }
+    }
+    /// <summary>
+    /// 스킬과 각 스킬의 갯수 정보를 담아낼 skillList 읽기 전용 
+    /// </summary>
     public List<SkillInfo> SkillList
     {
         get { return skills; }
+        
     }
 
     /// <summary>
@@ -49,7 +73,12 @@ public class GameManager : MonoBehaviour
         set
         {
             SkillInfo skill = skills[value];
-            skill.skillCnt = skill.skillCnt >= skillMaxCnt ? skillMaxCnt : skill.skillCnt + 1;
+            if(currentSkillCnt + 1 <= skillMaxCnt)
+            {
+                skill.skillCnt++;
+                skills[value] = skill;
+                currentSkillCnt++;
+            }
         }
     }
 
@@ -64,7 +93,12 @@ public class GameManager : MonoBehaviour
         set
         {
             SkillInfo skill = skills[value];
-            skill.skillCnt = skill.skillCnt >= skillMaxCnt ? skillMaxCnt : skill.skillCnt - 1;
+            if (currentSkillCnt - 1 >= 0)
+            {
+                skill.skillCnt--;
+                skills[value] = skill;
+                currentSkillCnt--;
+            }
         }
     }
 }
