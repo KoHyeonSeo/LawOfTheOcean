e7f4206d71e4c85669b1e45f74ed89808dc8e45b
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-06 16:25:07 +0900

    update

---
+++ Assets/Script/SkillOrb.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillOrb : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    private Transform player;
+    private Vector3 dir;
+    private bool isTouch = false;
+    public bool IsTouch { get { return isTouch; } set { isTouch = value; }  }
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = player.position + player.forward - transform.position ;
+        transform.position += speed * dir * Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsTouch = true;
+        }
+    }
+}
---
+++ Assets/Script/StolenSkill.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StolenSkill : MonoBehaviour
+{
+    [SerializeField] private GameObject skill;
+    private GameObject UsedOrb;
+    private bool first = true;
+    private void Update()
+    {
+        if (GetComponent<EnemyHealth>().DeadCheck && first)
+        {
+            if (GameManager.instance.IsStealUse)
+            {
+                UsedOrb = Instantiate(skill);
+                UsedOrb.transform.position = transform.position;
+                first = false;
+            }
+        }
+        if (!first)
+        {
+            if (UsedOrb.GetComponent<SkillOrb>().IsTouch)
+            {
+                Destroy(UsedOrb);
+                GetComponent<EnemyHealth>().EnemyDestroy();
+            }
+        }
+    }
+}
