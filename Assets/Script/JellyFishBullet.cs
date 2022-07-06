c5f2f063671c72674b3e6b4cd654e6e8a4aa77df
JasonShin10 <sjason2680@gmail.com>
2022-07-06 17:17:01 +0900

    .

--- Assets/Script/JellyFishBullet.cs
+++ Assets/Script/JellyFishBullet.cs
@@ -31,17 +31,33 @@ public class JellyFishBullet : MonoBehaviour
     {
         transform.position += dir * speed * Time.deltaTime;
     }
+    float currentTime;
+    [SerializeField] private float stopTime = 1.5f; 
     private void OnTriggerEnter(Collider other)
     {
+        
         //부딪힌 것이 스킬 사용자가 아니고 생명체(Entity)라면
         if (other.gameObject != user && other.gameObject.layer == 7)
         {
+
+            print("start");
             //Player라면 데미지 깎음
             //발사를 일정시간동안 못함
             if (other.gameObject.CompareTag("Player"))
             {
+                print("시작");
+                currentTime += Time.deltaTime;
                 other.gameObject.GetComponent<PlayerHealth>().Damage(damage);
-                
+
+               
+                GameManager.instance.IsStopAttack = false;
+                if (currentTime > stopTime )
+                {
+                    GameManager.instance.IsStopAttack = true;
+                    currentTime = 0;
+                    print("끝");
+                }
+
             }
             //Enemy라면 데미지 깎음
             else
