69065c56779c274d3d4ffa1d3fc52aaad818ba96
JasonShin10 <sjason2680@gmail.com>
2022-07-07 08:59:38 +0900

    .

--- Assets/Script/JellyFish/JellyFishBullet.cs
+++ Assets/Script/JellyFish/JellyFishBullet.cs
@@ -11,7 +11,7 @@ public class JellyFishBullet : MonoBehaviour
     public GameObject User { get { return user; } set { user = value; } }
     //데미지 즉, Power가 어느 정도인지 알 수 있는 프로퍼티
     public float BulletDamage { get { return damage; } set { damage = value; } }
-    
+
     public Transform target;
     Vector3 dir;
     // Start is called before the first frame update
@@ -25,48 +25,54 @@ public class JellyFishBullet : MonoBehaviour
         transform.rotation = rot;
 
     }
-
+    bool stop = false;
+    float currentTime;
+    [SerializeField] private float stopTime = 1;
     // Update is called once per frame
     void Update()
     {
+        if (stop == true)
+        {
+            currentTime += Time.deltaTime;
+                print("끝");
+            if (currentTime > stopTime)
+            {
+                currentTime = 0;
+                GameManager.instance.IsStopAttack = false;
+            }
+        }
+
+        print(stop);
         transform.position += dir * speed * Time.deltaTime;
     }
-    float currentTime;
-    [SerializeField] private float stopTime = 1.5f; 
     private void OnTriggerEnter(Collider other)
     {
-        
+
         //부딪힌 것이 스킬 사용자가 아니고 생명체(Entity)라면
         if (other.gameObject != user && other.gameObject.layer == 7)
         {
 
-            print("start");
+          ;
             //Player라면 데미지 깎음
             //발사를 일정시간동안 못함
             if (other.gameObject.CompareTag("Player"))
             {
                 print("시작");
-                currentTime += Time.deltaTime;
+                stop = true;
                 other.gameObject.GetComponent<PlayerHealth>().Damage(damage);
 
-               
-                GameManager.instance.IsStopAttack = false;
-                if (currentTime > stopTime )
-                {
-                    GameManager.instance.IsStopAttack = true;
-                    currentTime = 0;
-                    print("끝");
-                }
+                GameManager.instance.IsStopAttack = true;
 
             }
+
             //Enemy라면 데미지 깎음
             else
             {
                 other.gameObject.GetComponent<EnemyHealth>().Damage(damage);
             }
             //총알 삭제
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
-
     }
+
 }
