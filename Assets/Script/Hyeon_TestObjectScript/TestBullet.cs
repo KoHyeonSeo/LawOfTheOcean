19316f8b5085614d2728f27a0b2365adeee53e4e
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-07 11:19:03 +0900

    update

--- Assets/Script/Hyeon_TestObjectScript/TestBullet.cs
+++ Assets/Script/Hyeon_TestObjectScript/TestBullet.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class TestBullet : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private GameObject user;
     private float damage;
     //스킬을 사용하는 User가 누구인지 알 수 있는 프로퍼티
-    public GameObject User { get { return user; } set { user = value; } }
+    public GameObject User { get; set; }
     //데미지 즉, Power가 어느 정도인지 알 수 있는 프로퍼티
     public float BulletDamage { get { return damage; } set { damage = value; } }
     void Update()
@@ -19,7 +18,7 @@ public class TestBullet : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //부딪힌 것이 스킬 사용자가 아니고 생명체(Entity)라면
-        if (other.gameObject != user && other.gameObject.layer == 7)
+        if (other.gameObject != User && other.gameObject.layer == 7)
         {
             //Player라면 데미지 깎음
             if (other.gameObject.CompareTag("Player"))
