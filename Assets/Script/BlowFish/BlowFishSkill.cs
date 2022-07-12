6199fdb608c532a4339f2e397bb51a0b1439df48
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-12 17:38:10 +0900

    update

---
+++ Assets/Script/BlowFish/BlowFishSkill.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EnemySkill/BlowFishSkill")]
+public class BlowFishSkill : Skill
+{
+    [SerializeField] private GameObject bulletFactory;
+    //To do 
+    //1. 오브젝트 풀 만들기
+    //2. 다양한 각도에서 발사하기
+
+    public override void UseSkill()
+    {
+        GameObject bullet = Instantiate(bulletFactory);
+        bullet.GetComponent<ThornBullet>().BulletUser = User;
+        bullet.GetComponent<ThornBullet>().BulletDamage = Power;
+        bullet.transform.position = User.transform.position;
+    }
+}
