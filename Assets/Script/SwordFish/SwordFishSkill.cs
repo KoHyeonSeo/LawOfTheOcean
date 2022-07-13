e7c57bd356026c16e82cc86a0c1c04f445d18761
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-13 17:43:10 +0900

    update

---
+++ Assets/Script/SwordFish/SwordFishSkill.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EnemySkill/SwordFishSkill")]
+public class SwordFishSkill : Skill
+{
+    public override void UseSkill()
+    {
+        
+    }
+}
