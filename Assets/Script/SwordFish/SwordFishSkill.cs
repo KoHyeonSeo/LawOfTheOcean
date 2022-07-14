8f36dc4e92785552f0ef6dfaa20ad20ecddbe3c2
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-14 19:52:23 +0900

    updatw

--- Assets/Script/SwordFish/SwordFishSkill.cs
+++ Assets/Script/SwordFish/SwordFishSkill.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "EnemySkill/SwordFishSkill")]
 public class SwordFishSkill : Skill
 {
+    [SerializeField] private GameObject useSkillFactory;
+
     public override void UseSkill()
     {
-        
+        GameObject useSkill = Instantiate(useSkillFactory);
+        useSkill.GetComponent<SwordFishUseSkill>().UseSkill = true;
+        useSkill.GetComponent<SwordFishUseSkill>().SkillUser = User;
+        useSkill.GetComponent<SwordFishUseSkill>().SkillCoolTime = CoolTime;
     }
 }
