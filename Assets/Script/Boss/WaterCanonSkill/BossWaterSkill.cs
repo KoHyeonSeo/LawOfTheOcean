30f111bf9ffe0d412c4167ccc8c4aec7139ea35f
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-15 03:16:14 +0900

    update

---
+++ Assets/Script/Boss/WaterCanonSkill/BossWaterSkill.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EnemySkill/BossWaterSkill")]
+public class BossWaterSkill : Skill
+{
+    [SerializeField] private GameObject waterCanonPrefab;
+    [SerializeField] private int bulletCount = 100;
+    private Vector3 dir;
+    public override void UseSkill()
+    {
+        float deltaAngle = 100 / bulletCount;
+        dir = Vector3.up;
+        for (int i = 1; i <= bulletCount; i++)
+        {
+            GameObject water = Instantiate(waterCanonPrefab);
+
+            water.GetComponent<WaterBullet>().BulletUser = User;
+            water.GetComponent<WaterBullet>().BulletDamage = Power;
+            dir = Quaternion.Euler(0, 0, deltaAngle + 90) * dir;
+            water.transform.up = dir;
+            water.transform.position = User.transform.position;
+            water.GetComponent<Rigidbody>().AddForce(dir.normalized * 1000,ForceMode.Force);
+
+
+            GameObject water2 = Instantiate(waterCanonPrefab);
+
+            water2.GetComponent<WaterBullet>().BulletUser = User;
+            water2.GetComponent<WaterBullet>().BulletDamage = Power;
+            dir = Quaternion.Euler(0, deltaAngle + 90, 0) * dir;
+            water2.transform.up = dir;
+            water2.transform.position = User.transform.position;
+            water2.GetComponent<Rigidbody>().AddForce(dir.normalized * 1000, ForceMode.Force);
+
+
+            GameObject water3 = Instantiate(waterCanonPrefab);
+
+            water3.GetComponent<WaterBullet>().BulletUser = User;
+            water3.GetComponent<WaterBullet>().BulletDamage = Power;
+            dir = Quaternion.Euler(deltaAngle + 90, 0, 0) * dir;
+            water3.transform.up = dir;
+            water3.transform.position = User.transform.position;
+            water3.GetComponent<Rigidbody>().AddForce(dir.normalized * 1000, ForceMode.Force);
+
+        }
+    }
+
+}
