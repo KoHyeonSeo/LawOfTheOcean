43f6d28acf622c5d62be5863e8410d2180f4406a
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-07-17 23:49:27 +0900

    .

--- Assets/Script/Boss/StartTrigger.cs
+++ Assets/Script/Boss/StartTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartTrigger : MonoBehaviour
 {
@@ -24,6 +25,9 @@ public class StartTrigger : MonoBehaviour
         {
             other.gameObject.GetComponent<Boss>().first = false;
         }
-    
+    if (boss.GetComponent<EnemyHealth>().DeadCheck == true)
+        {
+            SceneManager.LoadScene("EndingScene");
+        }
     }
 }
