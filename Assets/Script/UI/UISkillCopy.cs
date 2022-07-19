eb6096b7ccc1ea4a625631185f98bcb8e9cc672e
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-07-19 17:13:33 +0900

    update Sce

--- Assets/Script/UI/UISkillCopy.cs
+++ Assets/Script/UI/UISkillCopy.cs
@@ -70,10 +70,10 @@ public class UISkillCopy : MonoBehaviour
         effect3.initial = skillEffect3.transform.position;
         effect4.initial = skillEffect4.transform.position;
 
-        effect1.afterPosition = skillEffect1.transform.position + new Vector3(0, 300, 0);
-        effect2.afterPosition = skillEffect2.transform.position + new Vector3(0, -300, 0);
-        effect3.afterPosition = skillEffect3.transform.position + new Vector3(-400, 0, 0);
-        effect4.afterPosition = skillEffect4.transform.position + new Vector3(400, 0, 0);
+        effect1.afterPosition = skillEffect1.transform.position + new Vector3(0, 500, 0);
+        effect2.afterPosition = skillEffect2.transform.position + new Vector3(0, -500, 0);
+        effect3.afterPosition = skillEffect3.transform.position + new Vector3(-500, 0, 0);
+        effect4.afterPosition = skillEffect4.transform.position + new Vector3(500, 0, 0);
 
         skillEffect1.transform.position = effect1.afterPosition;
         skillEffect2.transform.position = effect2.afterPosition;
