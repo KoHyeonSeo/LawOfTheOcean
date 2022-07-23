b50bf960451199e74f576821261df5df2a9f9cda
JasonShin10 <sjason2680@gmail.com>
2022-07-23 22:23:34 +0900

    .

--- Assets/Script/Player/PlayerCineMachine.cs
+++ Assets/Script/Player/PlayerCineMachine.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerCineMachine : MonoBehaviour
 {
+    float speed = 2;
+    Vector3 dir;
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject start = GameObject.Find("Start");
+        target = start.transform;
+        dir = target.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += dir * speed * Time.deltaTime;
     }
 }
