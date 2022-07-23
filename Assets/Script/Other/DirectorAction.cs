fa3ab9c7c3d16c1b3c4cd78db7fb185c65c0c153
Kohyeonseo <rhgustj01@naver.com>
2022-07-23 17:55:39 +0900

    update

---
+++ Assets/Script/Other/DirectorAction.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class DirectorAction : MonoBehaviour
+{
+    private PlayableDirector pd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pd = GetComponent<PlayableDirector>();
+        pd.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pd.duration - pd.time <= 0.01f)
+        {
+            SceneManager.LoadScene(6);
+        }
+    }
+}
