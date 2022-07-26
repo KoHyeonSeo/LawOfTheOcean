1bfa5f44623cd736b81a9038783d1c1fd50d3deb
Kohyeonseo <rhgustj01@naver.com>
2022-07-26 23:53:28 +0900

    Update UI_Hurt.cs

--- Assets/Script/UI/UI_Hurt.cs
+++ Assets/Script/UI/UI_Hurt.cs
@@ -41,8 +41,8 @@ public class UI_Hurt : MonoBehaviour
         curScreen.width = Screen.width;
         curScreen.height = Screen.height;
 
-        effect1.initial = new Vector3(Screen.width / 2, Screen.height - Screen.height / 8, 0);
-        effect2.initial = new Vector3(Screen.width / 2, Screen.height / 8, 0);
+        effect1.initial = new Vector3(Screen.width / 2, Screen.height - Screen.height / 10, 0);
+        effect2.initial = new Vector3(Screen.width / 2, Screen.height / 10, 0);
         effect3.initial = new Vector3(Screen.width / 17, Screen.height / 2, 0);
         effect4.initial = new Vector3(Screen.width - Screen.width / 17, Screen.height / 2, 0);
 
